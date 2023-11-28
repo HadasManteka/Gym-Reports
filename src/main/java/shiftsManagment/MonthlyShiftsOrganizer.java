61557150313b16f1e89c22ec75827b1114830925
Hadas <hadas716@gmail.com>
2023-11-28 22:29:23 +0200

    calculate month by four excels

--- src/main/java/shiftsManagment/MonthlyShiftsOrganizer.java
+++ src/main/java/shiftsManagment/MonthlyShiftsOrganizer.java
@@ -1,27 +1,64 @@
 package shiftsManagment;
 
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MonthlyShiftsOrganizer {
 
-    WeeklyShiftsOrganizer[] monthlyShiftsOrg = new WeeklyShiftsOrganizer[4];
+//    WeeklyShiftsOrganizer[] monthlyShiftsOrg = new WeeklyShiftsOrganizer[4];
     HashMap<String, Integer>[] countStudiosPerDay;
-
+    HashMap<String, Integer> studiosPerEmployee;
+    HashMap<String, Double> hoursPerEmployeeMap;
+    String[] paths = new String[4];
 
     public MonthlyShiftsOrganizer(String[] paths) {
-        for (int i=0; i<=4; i++) {
-            monthlyShiftsOrg[i] = new WeeklyShiftsOrganizer(paths[i]);
-        }
+        paths = paths;
+//        for (int i=0; i<=4; i++) {
+//            monthlyShiftsOrg[i] = new WeeklyShiftsOrganizer(paths[i]);
+//        }
     }
 
     public void calcAllMonth() throws ParseException {
         for (int i=0; i<=4; i++) {
-            monthlyShiftsOrg[i].calcAllReports();
-            monthlyShiftsOrg[i].getCountStudiosPerDay();
-            monthlyShiftsOrg[i].getStudiosPerEmployee();
+            WeeklyShiftsOrganizer weekOrg = new WeeklyShiftsOrganizer(paths[i]);
+            weekOrg.calcAllReports();
 
+            weekOrg.getCountStudiosPerDay();
+            mixTwoIntHash(studiosPerEmployee, weekOrg.getStudiosPerEmployee());
+            mixTwoDoubleHash(hoursPerEmployeeMap, weekOrg.getHoursPerEmployeeMap());
+            mixTwoIntArrayHash(countStudiosPerDay, weekOrg.getCountStudiosPerDay());
         }
     }
 
+    public HashMap<String, Integer> mixTwoIntHash(HashMap<String, Integer> firstHash,
+                                               HashMap<String, Integer> secondHash) {
+        secondHash.keySet().forEach(key-> {
+            if (firstHash.containsKey(key)) {
+                firstHash.put(key, firstHash.get(key) + secondHash.get(key));
+            }
+        });
+
+        return firstHash;
+    }
+
+    public HashMap<String, Double> mixTwoDoubleHash(HashMap<String, Double> firstHash,
+                                               HashMap<String, Double> secondHash) {
+        secondHash.keySet().forEach(key-> {
+            if (firstHash.containsKey(key)) {
+                firstHash.put(key, firstHash.get(key) + secondHash.get(key));
+            }
+        });
+
+        return firstHash;
+    }
+
+    public HashMap<String, Integer>[] mixTwoIntArrayHash(HashMap<String, Integer>[] firstArrHash,
+                                                    HashMap<String, Integer>[] secondArrHash) {
+        for (int i=0; i<=secondArrHash.length; i++) {
+            mixTwoIntHash(firstArrHash[i], secondArrHash[i]);
+        }
+
+        return firstArrHash;
+    }
 }
